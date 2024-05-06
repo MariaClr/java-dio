c2089d3ad02b5a305042919dc2818e5a235e5d5f
Maria Clara <134016322+MariaClr@users.noreply.github.com>
2024-05-06 11:14:10 -0300

    Desafios Dio
    Desafio 1

---
+++ Dio-Java/Desafio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Desafio1 {
+
+    public void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Login");
+        String nome = scanner.next();
+        System.out.println("senha");
+        String senha = scanner.next();
+        System.out.println("Conta criada com sucesso");
+
+    }
+}
